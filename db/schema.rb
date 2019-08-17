f2958560be2a1220b64971abcf89b3db1536b283
takumi aramaki <armktk0329m@gmail.com>
2019-08-18 02:12:39 +0900

    Add:usersテーブルにnameとprofileカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,8 @@ ActiveRecord::Schema.define(version: 2019_08_17_094032) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "profile"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
