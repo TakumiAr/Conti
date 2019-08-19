2c743f81384614defa3f85f1b727e551b592e85b
takumi aramaki <armktk0329m@gmail.com>
2019-08-19 17:04:10 +0900

    Delete: 不要な記述の削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
 Rails.application.routes.draw do
-  get '/about', :to => "home#about"
   root 'home#index'
   devise_for :users, :controllers => {
     :registrations => 'users/registrations',
     :sessions => 'users/sessions'   
   } 
   
-  devise_scope :user do
-    get "user/:id", :to => "users/registrations#detail"
-    get "signup", :to => "users/registrations#new"
-    get "login", :to => "users/sessions#new"
-    get "logout", :to => "users/sessions#destroy"
-  end
+  # devise_scope :user do
+  #   get "user/:id", :to => "users/registrations#detail"
+  #   get "signup", :to => "users/registrations#new"
+  #   get "login", :to => "users/sessions#new"
+  #   get "logout", :to => "users/sessions#destroy"
+  # end
   resources :users, only: [:show]
   resources :requests, only: [:index, :new, :show]
   resources :services, only: [:show]
+  get '/about', :to => "home#about"
 end
